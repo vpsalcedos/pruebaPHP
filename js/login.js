258b7b1e2362132e4f0950c8c951b890d44e2db5
vpsalcedos <vpsalcedos@bitbucket.org>
2015-04-07 08:17:47 -0500

    Login de usuarios defualt

---
+++ js/login.js
@@ -0,0 +1,25 @@
+
+function signin()
+      {
+
+        form=document.getElementById('signinForm');
+        
+        var user = document.getElementById('user').value;
+        var password = document.getElementById('password').value;
+
+        if (user == "comprador" && password ===user)
+        {
+            window.location.href ='html/comprador_catalogo_camiseta.html';
+            
+        }else if(user == "artista" && password ===user)
+        {
+            window.location.href ='html/artista_inic.html';
+            
+        }else{
+           window.location.href ='vabrik.html';
+            
+        }
+        
+        
+        
+      }
